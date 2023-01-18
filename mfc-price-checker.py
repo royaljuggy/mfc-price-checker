7fcea0d0d1b04257a074f002492c9534d4d1e352
Jacob UW <jmeksava@uwaterloo.ca>
2023-01-18 17:14:40 -0500

    Update output

--- mfc-price-checker.py
+++ mfc-price-checker.py
@@ -3,8 +3,16 @@ from bs4 import BeautifulSoup
 from yen_parser import parse_yen
 from currency_converter import CurrencyConverter
 
+# constants
 username = "SkillSwap"
 ROOT_URL = "https://myfigurecollection.net"
+
+# variables
+no_price_counter = 0
+cost_in_yen = 0
+num_items = 0
+num_prizes = 0
+
 # scrape user info page
 URL = ROOT_URL + "/profile/" + username
 page = requests.get(URL)
@@ -14,19 +22,14 @@ soup = BeautifulSoup(page.content, "html.parser")
 # find first container for figure icons
 figure_icons = soup.find("div", class_="item-icons")
 
-no_price_counter = 0
-cost_in_yen = 0
-# c = CurrencyConverter()
-
 for figure_icon in figure_icons:
-    # print(figure_icon, end="\n"*2)
+    num_items += 1
     URL = ROOT_URL + figure_icon.find("a", class_="item-root-0").get("href")
-    # print(URL)
     page = requests.get(URL)
-
     soup = BeautifulSoup(page.content, "html.parser")
 
     # scrape price for current figure (default price type = YEN)
+    
     try:
         price = soup.find("span", class_="item-price").text
         
@@ -37,9 +40,13 @@ for figure_icon in figure_icons:
 
 c = CurrencyConverter()
 
-print("The total cost:");
+print("\n=== Total cost: ===");
 print("\tin YEN: " + str(cost_in_yen))
 
 # convert from JPY to CAD and round to 2 decimal places
-print("\tin CAD: " + str(round(c.convert(cost_in_yen, 'JPY', 'CAD'), 2)))
-print("Number of figures/items with no price available: " + str(no_price_counter))
+print("\tin CAD: " + str(round(c.convert(cost_in_yen, 'JPY', 'CAD'), 2)) + "\n")
+
+print("=== Some statistics ===")
+print("\tFigures with no price available: " + str(no_price_counter))
+print("\tNumber of items: " + str(num_items) + "\n")
+#print("\tNumber of prize figures: " + str(num_items) + "\n")
